b8db52981c7257958d7f248056f5f513b85a91c6
Natesh Narain <nnaraindev@gmail.com>
2015-03-25 19:18:46 -0400

    added vector3 and vector 4 functions

--- include/SGL/Math/Vector3.h
+++ include/SGL/Math/Vector3.h
@@ -8,6 +8,37 @@ namespace sgl
 {
 	SGLCLASS Vector3
 	{
+	public:
+		/* Vector Components */
+		float x;
+		float y;
+		float z;
+
+		/* Constructors */
+		Vector3();
+		Vector3(float x, float y, float z);
+
+		~Vector3();
+
+		/*  */
+
+		float length() const;
+		Vector3& normalize();
+
+		float dot(const Vector3 &v);
+		Vector3& cross(const Vector3& v);
+
+		/* Operators */
+		Vector3 operator+(const Vector3& v);
+		Vector3 operator-(const Vector3& v);
+		Vector3 operator*(float s);
+		Vector3 operator/(float s);
+
+		Vector3& operator-();
+		Vector3& operator+=(const Vector3& v);
+		Vector3& operator-=(const Vector3& v);
+		Vector3& operator*=(float s);
+		Vector3& operator/=(float s);
 	};
 }
 
