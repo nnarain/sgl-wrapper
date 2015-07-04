31c2fdff76955bef0210d13eab930863b890378e
Natesh Narain <nnaraindev@gmail.com>
2015-07-04 00:17:29 -0400

    Added Color class

---
+++ src/Graphics/Color.cpp
@@ -0,0 +1,34 @@
+
+#include "SGL/Graphics/Color.h"
+
+using namespace sgl;
+
+
+Color::Color(float r, float g, float b) : Color(r, g, b, 1)
+{
+}
+
+
+Color::Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a)
+{
+}
+
+Color Color::operator+(Color& c)
+{
+	return Color(this->r + c.r, this->g + c.g, this->b + c.b, this->a + c.a);
+}
+
+Color Color::operator-(Color& c)
+{
+	return Color(this->r - c.r, this->g - c.g, this->b - c.b, this->a - c.a);
+}
+
+Color Color::operator*(Color& c)
+{
+	return Color(this->r * c.r, this->g * c.g, this->b * c.b, this->a * c.a);
+}
+
+Color Color::operator*(float value)
+{
+	return Color(this->r * value, this->g * value, this->b * value, this->a * value);
+}
